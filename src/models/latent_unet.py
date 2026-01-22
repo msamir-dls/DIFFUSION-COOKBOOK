edabb4ac389d76d908247f038ffe89339fae74a9
Samir Mishra <168301967+msamir-dls@users.noreply.github.com>
2026-01-23 02:20:13 +0530

    Update latent_unet.py

--- src/models/latent_unet.py
+++ src/models/latent_unet.py
@@ -1,76 +1,33 @@
 import torch
 import torch.nn as nn
-from .unet import DownSample, UpSample, ResBlock, TimeEmbedding
+from .unet import ResBlock, TimeEmbedding
 
 class LatentUNet(nn.Module):
+    """Simplified U-Net for 4-channel latents"""
     def __init__(self, config):
         super().__init__()
-        # --- CRITICAL: Use Latent Dim (4), not Image Channels (1) ---
-        in_channels = config['vae']['latent_dim']
+        in_channels = config['vae']['latent_dim'] # 4
+        base_channels = config['model']['base_channels']
         
-        self.base_channels = config['model']['base_channels']
-        self.timesteps = config['diffusion']['timesteps']
+        self.time_embed = TimeEmbedding(base_channels * 4)
+        self.init_conv = nn.Conv2d(in_channels, base_channels, 3, padding=1)
         
-        # Time Embedding
-        self.time_embed = TimeEmbedding(self.base_channels)
+        # Simple U-Net structure for 8x8 input (no downsampling needed really, but we do one pass)
+        self.down1 = ResBlock(base_channels, base_channels * 2, base_channels * 4)
+        self.mid = ResBlock(base_channels * 2, base_channels * 2, base_channels * 4)
+        self.up1 = ResBlock(base_channels * 4, base_channels, base_channels * 4) # 4*ch because concat
         
-        # Initial Conv
-        self.init_conv = nn.Conv2d(in_channels, self.base_channels, kernel_size=3, padding=1)
-        
-        # Downsampling
-        self.downs = nn.ModuleList()
-        ch = self.base_channels
-        channel_mults = config['model']['channel_mult'] # e.g. [1, 2]
-        
-        current_mult = 1
-        for i, mult in enumerate(channel_mults):
-            out_ch = self.base_channels * mult
-            for _ in range(config['model']['num_res_blocks']):
-                self.downs.append(ResBlock(ch, out_ch, self.base_channels))
-                ch = out_ch
-            
-            if i != len(channel_mults) - 1:
-                self.downs.append(DownSample(ch))
-                
-        # Middle
-        self.mid_block1 = ResBlock(ch, ch, self.base_channels)
-        self.mid_attn = nn.Identity() # Simplification for MNIST
-        self.mid_block2 = ResBlock(ch, ch, self.base_channels)
-        
-        # Upsampling
-        self.ups = nn.ModuleList()
-        for i, mult in reversed(list(enumerate(channel_mults))):
-            out_ch = self.base_channels * mult
-            for _ in range(config['model']['num_res_blocks'] + 1):
-                self.ups.append(ResBlock(ch + out_ch, out_ch, self.base_channels)) # concat skip
-                ch = out_ch
-            
-            if i != 0:
-                self.ups.append(UpSample(ch))
-                
-        # Final
-        self.final_conv = nn.Conv2d(ch, in_channels, kernel_size=3, padding=1)
+        self.final = nn.Conv2d(base_channels, in_channels, 3, padding=1)
 
     def forward(self, x, t):
-        # Time
-        t_emb = self.time_embed(t)
+        t = self.time_embed(t)
+        x = self.init_conv(x)      # [B, 64, 8, 8]
         
-        # Initial
-        x = self.init_conv(x)
+        h1 = self.down1(x, t)      # [B, 128, 8, 8]
+        h_mid = self.mid(h1, t)    # [B, 128, 8, 8]
         
-        # Down
-        skips = []
-        for layer in self.downs:
-            if isinstance(layer, ResBlock):
-                x = layer(x, t_emb)
-            elif isinstance(layer, DownSample):
-                x = layer(x)
-                skips.append(x)
-            else:
-                x = layer(x)
+        # Concat skip connection
+        h_up = torch.cat([h_mid, h1], dim=1) 
+        h_up = self.up1(h_up, t)   # [B, 64, 8, 8]
         
-        # Mid
-        x = self.mid_block1(x, t_emb)
-        x = self.mid_block2(x, t_emb)
-        
-        return self.final_conv(x)
+        return self.final(h_up)
